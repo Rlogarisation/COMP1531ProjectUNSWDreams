c0a50061c8a8fb3b75e0cb89c1760021cda0c1f1
Lan Lin <497152365@qq.com>
2021-03-02 02:02:23 +1100

    1/3-6

--- src/other.py
+++ src/other.py
@@ -18,13 +18,3 @@ def search_v1(auth_user_id, query_str):
             }
         ],
     }
-
-
-register1 = auth_register_v1('haha@gmail.com', '123', 'Peter', 'White')
-register2 = auth_register_v1('test@testexample.com', 'wp01^#$dp1o23', 'Tom', 'Green')
-auth_user_id1 = register1['auth_user_id']
-auth_user_id2 = register2['auth_user_id']
-
-user1 = get_class_user(auth_user_id1)
-user2 = get_class_user(auth_user_id2)
-print(user1.handle_str)
