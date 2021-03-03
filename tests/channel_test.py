d80bc0dbc647695546857eaaa3072b09d86da4a9
yst990102 <styuan201099@gmail.com>
2021-03-03 15:44:08 +0800

    export random which is unused now

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -11,8 +11,6 @@ from src.error import InputError, AccessError
 # Imports pytest
 import pytest
 
-import random
-
 
 """
 Author : Emir Aditya Zen
@@ -43,25 +41,25 @@ def test_channel_invite_v1_success():
     # Clears data and registers and logins user_1 and user_2
     clear_v1()
     # register two users with valid inputs
-    auth_register_v1('haha@gmail.com', '123123123', 'Peter', 'White')
-    auth_register_v1('test@testexample.com', 'wp01^#$dp1o23', 'Tom', 'Green')
+    auth_register_v1("haha@gmail.com", "123123123", "Peter", "White")
+    auth_register_v1("test@testexample.com", "wp01^#$dp1o23", "Tom", "Green")
 
     # login the two registered users
-    login1 = auth_login_v1('haha@gmail.com', '123123123')
-    login2 = auth_login_v1('test@testexample.com', 'wp01^#$dp1o23')
+    login1 = auth_login_v1("haha@gmail.com", "123123123")
+    login2 = auth_login_v1("test@testexample.com", "wp01^#$dp1o23")
 
     # Identify user_id and auth_user_id for 2 registered user for testing
 
-    user_1_id_auth = login1['auth_user_id']
-    user_2_id_auth = login2['auth_user_id']
+    user_1_id_auth = login1["auth_user_id"]
+    user_2_id_auth = login2["auth_user_id"]
     user1 = get_user_by_auth_id(user_1_id_auth)
     user2 = get_user_by_auth_id(user_2_id_auth)
     user_2_id = user2.u_id
     user_1_id = user1.u_id
 
     # Create Channel_1 made by user_1 and get its id
-    create_channel1 = channels_create_v1(user_1_id_auth, 'channelone', True)
-    Channel_1_id = create_channel1['channel_id']
+    create_channel1 = channels_create_v1(user_1_id_auth, "channelone", True)
+    Channel_1_id = create_channel1["channel_id"]
 
     # Calls invite function for testing
     # (user 1 invites user 2 into the channel he/she is in)
@@ -84,12 +82,12 @@ def test_channel_invite_v1_success():
 def test_channel_invite_v1_repeated():
     # Clears data and registers and logins user_1 and user_2
     clear_v1()
-    auth_register_v1('haha@gmail.com', '123123123', 'Peter', 'White')
-    auth_register_v1('test@testexample.com', 'wp01^#$dp1o23', 'Tom', 'Green')
+    auth_register_v1("haha@gmail.com", "123123123", "Peter", "White")
+    auth_register_v1("test@testexample.com", "wp01^#$dp1o23", "Tom", "Green")
 
     # login the two registered users
-    auth_id1 = auth_login_v1('haha@gmail.com', '123123123')['auth_user_id']
-    auth_id2 = auth_login_v1('test@testexample.com', 'wp01^#$dp1o23')['auth_user_id']
+    auth_id1 = auth_login_v1("haha@gmail.com", "123123123")["auth_user_id"]
+    auth_id2 = auth_login_v1("test@testexample.com", "wp01^#$dp1o23")["auth_user_id"]
 
     user1 = get_user_by_auth_id(auth_id1)
     user2 = get_user_by_auth_id(auth_id2)
@@ -99,7 +97,7 @@ def test_channel_invite_v1_repeated():
     user_2_id = user2.u_id
 
     # Create Channel_1 made by user_1 and get its id
-    Channel_1_id = channels_create_v1(auth_id1, 'channelone', True)['channel_id']
+    Channel_1_id = channels_create_v1(auth_id1, "channelone", True)["channel_id"]
 
     # Calls invite function twice targeting the same user for testing repetition
     # (user 1 invites user 2 into the channel he/she is in twice)
@@ -124,18 +122,18 @@ def test_channel_invite_v1_repeated():
 def test_channel_invite_v1_inputErrorChannel():
     # Clears data and registers and logins user_1 and user_2
     clear_v1()
-    auth_register_v1('haha@gmail.com', '123123123', 'Peter', 'White')
-    auth_register_v1('test@testexample.com', 'wp01^#$dp1o23', 'Tom', 'Green')
+    auth_register_v1("haha@gmail.com", "123123123", "Peter", "White")
+    auth_register_v1("test@testexample.com", "wp01^#$dp1o23", "Tom", "Green")
 
     # login the two registered users
-    auth_id1 = auth_login_v1('haha@gmail.com', '123123123')['auth_user_id']
-    auth_id2 = auth_login_v1('test@testexample.com', 'wp01^#$dp1o23')['auth_user_id']
+    auth_id1 = auth_login_v1("haha@gmail.com", "123123123")["auth_user_id"]
+    auth_id2 = auth_login_v1("test@testexample.com", "wp01^#$dp1o23")["auth_user_id"]
 
     user2 = get_user_by_auth_id(auth_id2)
     user_2_id = user2.u_id
 
     # Create Channel_1 made by user_1 and get its id
-    Channel_1_id = channels_create_v1(auth_id1, 'channelone', True)['channel_id']
+    Channel_1_id = channels_create_v1(auth_id1, "channelone", True)["channel_id"]
 
     # Made an invalid channel id for testing
     Channel_1_invalid_id = Channel_1_id + 300
@@ -151,18 +149,18 @@ def test_channel_invite_v1_inputErrorChannel():
 def test_channel_invite_v1_inputErrorUser():
     # Clears data and registers and logins user_1 and user_2
     clear_v1()
-    auth_register_v1('haha@gmail.com', '123123123', 'Peter', 'White')
-    auth_register_v1('test@testexample.com', 'wp01^#$dp1o23', 'Tom', 'Green')
+    auth_register_v1("haha@gmail.com", "123123123", "Peter", "White")
+    auth_register_v1("test@testexample.com", "wp01^#$dp1o23", "Tom", "Green")
 
     # login the two registered users
-    auth_id1 = auth_login_v1('haha@gmail.com', '123123123')['auth_user_id']
-    auth_id2 = auth_login_v1('test@testexample.com', 'wp01^#$dp1o23')['auth_user_id']
+    auth_id1 = auth_login_v1("haha@gmail.com", "123123123")["auth_user_id"]
+    auth_id2 = auth_login_v1("test@testexample.com", "wp01^#$dp1o23")["auth_user_id"]
 
     user2 = get_user_by_auth_id(auth_id2)
     user_2_id = user2.u_id
 
     # Create Channel_1 made by user_1 and get its id
-    Channel_1_id = channels_create_v1(auth_id1, 'channelone', True)['channel_id']
+    Channel_1_id = channels_create_v1(auth_id1, "channelone", True)["channel_id"]
 
     # Made an invalid user id for user 2
     user_2_id_invalid_id = user_2_id + 300
@@ -179,20 +177,20 @@ def test_channel_invite_v1_inputErrorUser():
 def test_channel_invite_v1_accessError():
     # Clears data and registers and logins user_1, user_2, and user_3
     clear_v1()
-    auth_register_v1('haha@gmail.com', '123123123', 'Peter', 'White')
-    auth_register_v1('test@testexample.com', 'wp01^#$dp1o23', 'Tom', 'Green')
-    auth_register_v1('hah2@gmail.com', '9uisbxh83h', 'Tom', 'Green')
+    auth_register_v1("haha@gmail.com", "123123123", "Peter", "White")
+    auth_register_v1("test@testexample.com", "wp01^#$dp1o23", "Tom", "Green")
+    auth_register_v1("hah2@gmail.com", "9uisbxh83h", "Tom", "Green")
 
     # login the two registered users
-    auth_id1 = auth_login_v1('haha@gmail.com', '123123123')['auth_user_id']
-    auth_id2 = auth_login_v1('test@testexample.com', 'wp01^#$dp1o23')['auth_user_id']
-    auth_id3 = auth_login_v1('hah2@gmail.com', '9uisbxh83h')['auth_user_id']
+    auth_id1 = auth_login_v1("haha@gmail.com", "123123123")["auth_user_id"]
+    auth_id2 = auth_login_v1("test@testexample.com", "wp01^#$dp1o23")["auth_user_id"]
+    auth_id3 = auth_login_v1("hah2@gmail.com", "9uisbxh83h")["auth_user_id"]
 
     user3 = get_user_by_auth_id(auth_id3)
     user_3_id = user3.u_id
 
     # Create Channel_1 made by user_1 and get its id
-    Channel_1_id = channels_create_v1(auth_id1, 'channelone', True)['channel_id']
+    Channel_1_id = channels_create_v1(auth_id1, "channelone", True)["channel_id"]
 
     # Test conditions leading to an access error outcome
     with pytest.raises(AccessError):
@@ -226,28 +224,28 @@ AccessError when any of:
 def test_channel_details_v1_success():
     # Clears data and registers and logins user_1
     clear_v1()
-    auth_register_v1('haha@gmail.com', '123123123', 'Peter', 'White')
-    auth_register_v1('test@testexample.com', 'wp01^#$dp1o23', 'Tom', 'Green')
+    auth_register_v1("haha@gmail.com", "123123123", "Peter", "White")
+    auth_register_v1("test@testexample.com", "wp01^#$dp1o23", "Tom", "Green")
 
     # login the two registered users
-    auth_id1 = auth_login_v1('haha@gmail.com', '123123123')['auth_user_id']
-    auth_id2 = auth_login_v1('test@testexample.com', 'wp01^#$dp1o23')['auth_user_id']
+    auth_id1 = auth_login_v1("haha@gmail.com", "123123123")["auth_user_id"]
+    auth_id2 = auth_login_v1("test@testexample.com", "wp01^#$dp1o23")["auth_user_id"]
 
     user2 = get_user_by_auth_id(auth_id2)
     user1 = get_user_by_auth_id(auth_id1)
     user_2_id = user2.u_id
 
     # Create Channel_1 made by user_1 and get its id
-    Channel_1_id = channels_create_v1(auth_id1, 'channelone', True)['channel_id']
+    Channel_1_id = channels_create_v1(auth_id1, "channelone", True)["channel_id"]
 
     channel_invite_v1(auth_id1, Channel_1_id, user_2_id)
     # Calls details function for testing
     output = channel_details_v1(auth_id1, Channel_1_id)
 
-    assert output['all_members'][0]['name_first'] == user1.name_first
-    assert output['all_members'][1]['name_first'] == user2.name_first
-    assert output['owner_members'][0]['name_first'] == user1.name_first
-    assert output['name'] == 'channelone'
+    assert output["all_members"][0]["name_first"] == user1.name_first
+    assert output["all_members"][1]["name_first"] == user2.name_first
+    assert output["owner_members"][0]["name_first"] == user1.name_first
+    assert output["name"] == "channelone"
 
 
 # Case 3 - tests for input error due to invalid channel
@@ -256,14 +254,14 @@ def test_channel_details_v1_success():
 def test_channel_details_v1_inputError():
     # Clears data and registers and logins user_1 and user_2
     clear_v1()
-    auth_register_v1('haha@gmail.com', '123123123', 'Peter', 'White')
-    auth_register_v1('test@testexample.com', 'wp01^#$dp1o23', 'Tom', 'Green')
+    auth_register_v1("haha@gmail.com", "123123123", "Peter", "White")
+    auth_register_v1("test@testexample.com", "wp01^#$dp1o23", "Tom", "Green")
 
     # login the two registered users
-    auth_id1 = auth_login_v1('haha@gmail.com', '123123123')['auth_user_id']
+    auth_id1 = auth_login_v1("haha@gmail.com", "123123123")["auth_user_id"]
 
     # Create Channel_1 made by user_1 and get its id
-    Channel_1_id = channels_create_v1(auth_id1, 'channelone', True)['channel_id']
+    Channel_1_id = channels_create_v1(auth_id1, "channelone", True)["channel_id"]
 
     # Made an invalid channel id for testing
     Channel_1_invalidid = Channel_1_id + 300
@@ -279,20 +277,19 @@ def test_channel_details_v1_inputError():
 def test_channel_details_v1_accessError():
     # Clears data and registers and logins user_1 and user_2
     clear_v1()
-    auth_register_v1('haha@gmail.com', '123123123', 'Peter', 'White')
-    auth_register_v1('test@testexample.com', 'wp01^#$dp1o23', 'Tom', 'Green')
+    auth_register_v1("haha@gmail.com", "123123123", "Peter", "White")
+    auth_register_v1("test@testexample.com", "wp01^#$dp1o23", "Tom", "Green")
 
     # login the two registered users
-    auth_id1 = auth_login_v1('haha@gmail.com', '123123123')['auth_user_id']
-    auth_id2 = auth_login_v1('test@testexample.com', 'wp01^#$dp1o23')['auth_user_id']
+    auth_id1 = auth_login_v1("haha@gmail.com", "123123123")["auth_user_id"]
+    auth_id2 = auth_login_v1("test@testexample.com", "wp01^#$dp1o23")["auth_user_id"]
 
     # Create Channel_1 made by user_1 and get its id
-    Channel_1_id = channels_create_v1(auth_id1, 'channelone', True)['channel_id']
+    Channel_1_id = channels_create_v1(auth_id1, "channelone", True)["channel_id"]
 
     # Test conditions leading to an access error outcome
     with pytest.raises(AccessError):
         channel_details_v1(auth_id2, Channel_1_id)
-
 
 
 """
@@ -309,6 +306,7 @@ Error:
 - the auth user is not in this 
 
 """
+
 
 def msg_send(channel_id, msg_id, u_id, msg, time):
     message = {
@@ -518,7 +516,7 @@ def test_invalid_channel_id():
     assert type(channel_id["channel_id"]) is int
 
     # Test for invalid channel id
-    invalid_id = {"channel_id": random.randint(0, 10)}
+    invalid_id = {"channel_id": 999999}
     with pytest.raises(InputError):
         channel_join_v1(joiner_auth_id, invalid_id["channel_id"])
 
