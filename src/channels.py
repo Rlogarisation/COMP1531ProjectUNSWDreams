4b5021ab38a718081f226d80368b057b2e987687
Zheng Luo <z5206267@ad.unsw.edu.au>
2021-03-03 14:11:51 +1100

    Simplied the channels function, make it more readble

--- src/channels.py
+++ src/channels.py
@@ -37,14 +37,7 @@ def channels_list_v1(auth_user_id):
     list_return = []
     for channel in user.part_of_channel:
         if (channel.is_public == True):
-            channel_id_re = channel.channel_id
-            channel_name_re = channel.name
-            dict_re = {
-                'channel_id': channel_id_re,
-                'name': channel_name_re
-            }
-            list_return.append(dict_re)
-
+            list_return.append(channel.return_type_channel())
     return list_return
 
 #############################################################################
@@ -72,14 +65,7 @@ def channels_listall_v1(auth_user_id):
     # Call return_type_channel(self) in order to get dictionary return
     list_return = []
     for channel in user.part_of_channel:
-        channel_id_re = channel.channel_id
-        channel_name_re = channel.name
-        dict_re = {
-            'channel_id': channel_id_re,
-            'name': channel_name_re
-        }
-        list_return.append(dict_re)
-
+        list_return.append(channel.return_type_channel())
     return list_return
 
 
@@ -97,11 +83,9 @@ def channels_create_v1(auth_user_id, name, is_public):
     if not isinstance(is_public, bool):
         raise InputError('is_public has to be bool')
     # error check if the owner has registered
-    
     owner = get_user_by_auth_id(auth_user_id)
     if owner is None:
         raise InputError('The owner has not registered')
-    
     # get the owner who creates the channel by auth_user_id
     channel_id = create_channel_id()
     channel = Channel(name, channel_id, is_public)
@@ -116,38 +100,7 @@ def channels_create_v1(auth_user_id, name, is_public):
     }
 
 
-if __name__ == '__main__':
-    clear_v1()
-    auth_register_v1('haha@gmail.com', '123123123', 'Peter', 'White')
-    login = auth_login_v1('haha@gmail.com', '123123123')
-    auth_user_id = login['auth_user_id']
-    owner = get_user_by_auth_id(auth_user_id)
 
-    channel1_id = channels_create_v1(auth_user_id, "public_channel", True)['channel_id']
-    channel2_id = channels_create_v1(auth_user_id, "private_channel", False)['channel_id']
-
-    channel1 = get_channel_by_channel_id(channel1_id)
-    channel2 = get_channel_by_channel_id(channel2_id)
-    assert channel2 is not None
-    # print(f"len is {len(owner.part_of_channel)}")
-    # print('hah')
-    # print(channel1.name)
-    # print('hah')
-    # print(owner.part_of_channel[1].name)
-    #
-    # print(owner.part_of_channel)
-
-    assert channel1 in owner.part_of_channel
-    assert channel2 in owner.part_of_channel
-    assert len(owner.part_of_channel) == 2
-    assert channel1 in owner.channel_owns
-    assert channel2 in owner.channel_owns
-    assert len(owner.channel_owns) == 2
-
-    assert owner in channel1.all_members
-    assert owner in channel1.owner_members
-    assert owner in channel2.all_members
-    assert owner in channel2.owner_members
 
 
 
