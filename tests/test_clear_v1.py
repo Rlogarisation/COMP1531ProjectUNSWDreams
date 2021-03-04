2767441399ad8a221121a8b826cb58229aaf1d72
Shaozhen Yan <shaozhen.yan@student.unsw.edu.au>
2021-03-05 00:00:40 +1100

    Update test_clear_v1.py

--- tests/test_clear_v1.py
+++ tests/test_clear_v1.py
@@ -2,7 +2,7 @@ import pytest
 from src.data_file import data
 from src.channels import channels_create_v1
 from src.other import clear_v1
-from src.auth import auth_register_v1
+from src.auth import auth_register_v1, get_user_by_auth_id
 
 
 def test_clear_v1():
@@ -13,15 +13,18 @@ def test_clear_v1():
     register = auth_register_v1('user@gmail.com', 'qwe1212', 'shaozhen', 'yan')
     user_id = register['auth_user_id']
     #create a channel 
-    channel_id = channels_create_v1(user_id, 'test_channel', True)['channel_id']
+    channel_id = channels_create_v1(user_id, 'test_channel', True)
     #check if the user and channel are created correctly in the empty dict
     assert data['class_channels'][0].name == 'test_channel'
     assert data['class_channels'][0].channel_id == channel_id 
     assert data['class_channels'][0].is_public == True
-    assert data['class_user'][0].u_id == user_id
-    assert data['class_user'][0].email == 'user@gmail.com'
-    assert data['class_user'][0].name_first == 'shaozhen'
-    assert data['class_user'][0].name_last == 'yan'
+    
+    user1 = get_user_by_auth_id(user_id)
+    
+    assert user1[0].u_id == user_id
+    assert user1[0].email == 'user@gmail.com'
+    assert user1[0].name_first == 'shaozhen'
+    assert user1[0].name_last == 'yan'
     
     #clear the information we created and check the validity of clear_v1
     clear_v1()
