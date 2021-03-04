cff33a63d9c47dfad21a9f4571b2190f376fd12f
Shaozhen Yan <shaozhen.yan@student.unsw.edu.au>
2021-03-04 23:51:01 +1100

    Update test_clear_v1.py

--- tests/test_clear_v1.py
+++ tests/test_clear_v1.py
@@ -12,7 +12,6 @@ def test_clear_v1():
     #create an user with details to run test
     register = auth_register_v1('user@gmail.com', 'qwe1212', 'shaozhen', 'yan')
     user_id = register['auth_user_id']
-
     #create a channel 
     channel_id = channels_create_v1(user_id, 'test_channel', True)
     #check if the user and channel are created correctly in the empty dict
@@ -20,6 +19,9 @@ def test_clear_v1():
     assert data['class_channels'][0].channel_id == channel_id 
     assert data['class_channels'][0].is_public == True
     assert data['class_user'][0].u_id == user_id
+    assert data['class_user'][0].email == 'user@gmail.com'
+    assert data['class_user'][0].name_first == 'shaozhen'
+    assert data['class_user'][0].name_last == 'yan'
     
     #clear the information we created and check the validity of clear_v1
     clear_v1()
