ca4a0f689c04e0aadaf3cc12921901eab2ed7a3a
Shaozhen Yan <shaozhen.yan@student.unsw.edu.au>
2021-03-04 18:40:53 +1100

    Upload test_clear_v1.py

---
+++ tests/test_clear_v1.py
@@ -0,0 +1,28 @@
+import pytest
+from src.data_file import data
+from src.channels import channels_create_v1
+from src.other import clear_v1
+from src.auth import auth_register_v1
+
+
+def test_clear_v1():
+    #clear all of information to run test
+    clear_v1()
+
+    #create an user with details to run test
+    user_id = auth_register_v1('user@gmail.com', 'qwe1212', 'shaozhen', 'yan')
+    #create a channel 
+    channel_id = channels_create_v1(user_id, 'test_channel', True)
+    #check if the user and channel are created correctly in the empty dict
+    assert data['class_channels'][0].name == 'test_channel'
+    assert data['class_channels'][0].channel_id == channel_id 
+    assert data['class_channels'][0].is_public == True
+    assert data['class_user'][0].u_id == user_id
+    
+    #clear the information we created and check the validity of clear_v1
+    clear_v1()
+    assert data['class_channels'] == []
+    assert data['class_user'] == []
+    
+    
+    
