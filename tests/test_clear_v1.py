e879354b04c24cd01d5f3bb34baff693fc9afda2
Shaozhen Yan <shaozhen.yan@student.unsw.edu.au>
2021-03-05 00:22:50 +1100

    Update test_clear_v1.py

--- tests/test_clear_v1.py
+++ tests/test_clear_v1.py
@@ -28,8 +28,7 @@ def test_clear_v1():
     
     #clear the information we created and check the validity of clear_v1
     clear_v1()
-    assert data['class_channels'] == []
-    assert data['class_user'] == []
+    assert data['class_channels'][0].name is None
     
     
     
