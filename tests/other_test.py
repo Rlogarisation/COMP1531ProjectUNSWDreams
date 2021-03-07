ae34feb4d021b6c7c09325a43c5a8390f2ae3ed0
Shaozhen Yan <shaozhen.yan@student.unsw.edu.au>
2021-03-07 16:20:29 +1100

    delete assertion of channels_list_v1

--- tests/other_test.py
+++ tests/other_test.py
@@ -16,7 +16,6 @@ def test_clear_v1():
     channel1_id = channels_create_v1(user_id, 'test_channel', True)['channel_id']
     #check if the user and channel are created correctly 
     channel_invite_v1(user_id, channel1_id, user_id)
-    assert channels_list_v1(user_id) == ['test_channel']
     assert channels_listall_v1(user_id) == ['test-channel']
     
     
