e9be9b2beb5962d02561712c26d29eb22b8a648f
Shaozhen Yan <shaozhen.yan@student.unsw.edu.au>
2021-03-07 16:59:21 +1100

    Modify the test_clear_v1 to ensure it doesn't depend on data.file

--- tests/other_test.py
+++ tests/other_test.py
@@ -4,6 +4,19 @@ from src.channels import channels_create_v1, channels_list_v1, channels_listall_
 from src.other import clear_v1
 from src.auth import auth_register_v1
 
+"""
+Author : Shaozhen Yan
+
+This file is for testing clear_v1 function implementation
+
+Background
+Resets the internal data of the application to it's initial state
+
+Parameters: ()
+Return Type: {}
+
+"""
+
 
 def test_clear_v1():
     #clear all of information to run test
@@ -25,6 +38,7 @@ def test_clear_v1():
     
     #clear the information we created and check the validity of clear_v1
     clear_v1()
+    assert user_id is None
     register = auth_register_v1('user@gmail.com', 'qwe1212', 'shaozhen', 'yan')
     user_id = register['auth_user_id']
     assert channels_listall_v1(user_id) == []
