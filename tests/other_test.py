513ec316bebee80d468b260f8483c79b090eab4e
Shaozhen Yan <shaozhen.yan@student.unsw.edu.au>
2021-03-07 16:17:10 +1100

    Delete all data.file associated test cases

--- tests/other_test.py
+++ tests/other_test.py
@@ -1,8 +1,8 @@
 import pytest
-from src.data_file import data
-from src.channels import channels_create_v1
+from src.channel import channel_invite_v1
+from src.channels import channels_create_v1, channels_list_v1, channels_listall_v1
 from src.other import clear_v1
-from src.auth import auth_register_v1, get_user_by_auth_id
+from src.auth import auth_register_v1
 
 
 def test_clear_v1():
@@ -14,19 +14,12 @@ def test_clear_v1():
     user_id = register['auth_user_id']
     #create a channel 
     channel1_id = channels_create_v1(user_id, 'test_channel', True)['channel_id']
-    #check if the user and channel are created correctly in the empty dict
-    assert data['class_channels'][0].name == 'test_channel'
-    assert data['class_channels'][0].channel_id == channel1_id 
-    assert data['class_channels'][0].is_public == True
+    #check if the user and channel are created correctly 
+    channel_invite_v1(user_id, channel1_id, user_id)
+    assert channels_list_v1(user_id) == ['test_channel']
+    assert channels_listall_v1(user_id) == ['test-channel']
     
-    user1 = get_user_by_auth_id(user_id)
-    
-    assert user1.u_id == user_id
-    assert user1.email == 'user@gmail.com'
-    assert user1.name_first == 'shaozhen'
-    assert user1.name_last == 'yan'
     
     #clear the information we created and check the validity of clear_v1
     clear_v1()
-    assert data['class_channels'] == []
-    assert data['class_users'] == []
+    assert channels_listall_v1(user_id) == []
