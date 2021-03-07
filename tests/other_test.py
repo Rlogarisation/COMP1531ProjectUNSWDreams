dc9a1340e1c401036557c6427540ec84d5942149
Shaozhen Yan <shaozhen.yan@student.unsw.edu.au>
2021-03-07 16:49:15 +1100

    Update other_test.py

--- tests/other_test.py
+++ tests/other_test.py
@@ -12,14 +12,17 @@ def test_clear_v1():
     #create an user with details to run test
     register = auth_register_v1('user@gmail.com', 'qwe1212', 'shaozhen', 'yan')
     user_id = register['auth_user_id']
+    
     #create channels
     channel0_id = channels_create_v1(user_id, 'test_public_channel', True)['channel_id']
     channel1_id = channels_create_v1(user_id, 'test_private_channel', False)['channel_id']
+    
     #check if the user and channel are created correctly 
     channel_invite_v1(user_id, channel0_id, user_id)
     channel_invite_v1(user_id, channel1_id, user_id)
     assert channels_listall_v1(user_id)[0] == {'channel_id': 0, 'name': 'test_public_channel'}
     assert channels_listall_v1(user_id)[1] == {'channel_id': 1, 'name': 'test_private_channel'}
+    
     #clear the information we created and check the validity of clear_v1
     clear_v1()
-    assert channels_listall_v1(user_id)[0] == {}
+    assert channels_listall_v1(user_id)[0] == []
