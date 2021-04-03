cf51e9758593317f1e6b2d12dd8e1f4182c8c484
Emir <z5223771@ad.unsw.edu.au>
2021-04-03 16:22:17 +1100

    Updated http test for channel and channels with working test expect channel_messages

--- http_tests/channel_http_test.py
+++ http_tests/channel_http_test.py
@@ -214,6 +214,23 @@ def test_channel_join_invalid_token_http(user1,user2):
 #############################################################################
 
 
+def test_channel_join_global_owner_exception_http(user1,user2):
+    requests.delete(config.url + 'clear/v1')
+    user1_register = requests.post(config.url + 'auth/register/v2', json=user1)
+    user2_register = requests.post(config.url + 'auth/register/v2', json=user2)
+    user1_login = requests.post(config.url + 'auth/login/v2', json=user1)
+    user2_login = requests.post(config.url + 'auth/login/v2', json=user2)
+    token1 = json.loads(user1_login.text).get('token')
+    token2 = json.loads(user2_login.text).get('token')
+    channel = requests.post(config.url + 'channels/create/v2', json={"token":token2,"name":"channelone","is_public":False})
+    channel_id = json.loads(channel.text).get('channel_id')
+    output = requests.post(config.url + 'channel/join/v2', json={"token":token1,"channel_id":channel_id}).status_code
+    assert output == 200
+    output2 = requests.get(config.url + 'channel/details/v2' + f'?token={token2}&channel_id={channel_id}')
+    assert output2.status_code == 200
+    assert len(json.loads(output2.text)['all_members']) == 2
+
+
 #############################################################################
 #                                                                           #
 #                     http test for channel_addowner Error                  #
@@ -298,6 +315,28 @@ def test_channel_addowner_invalid_token_http(user1,user2):
 #                                                                           #
 #############################################################################
 
+
+def test_channel_addowner_global_owner_exception_http(user1,user2,user3):
+    requests.delete(config.url + 'clear/v1')
+    user1_register = requests.post(config.url + 'auth/register/v2', json=user1)
+    user2_register = requests.post(config.url + 'auth/register/v2', json=user2)
+    user3_register = requests.post(config.url + 'auth/register/v2', json=user3)
+    user1_login = requests.post(config.url + 'auth/login/v2', json=user1)
+    user2_login = requests.post(config.url + 'auth/login/v2', json=user2)
+    user3_login = requests.post(config.url + 'auth/login/v2', json=user3)
+    token1 = json.loads(user1_login.text).get('token')
+    token2 = json.loads(user2_login.text).get('token')
+    token3 = json.loads(user3_login.text).get('token')
+    u_id3 = json.loads(user3_login.text).get('auth_user_id')
+    channel = requests.post(config.url + 'channels/create/v2', json={"token":token2,"name":"channelone","is_public":True})
+    channel_id = json.loads(channel.text).get('channel_id')
+    requests.post(config.url + 'channel/join/v2', json={"token":token3,"channel_id":channel_id}).status_code
+    output = requests.post(config.url + 'channel/addowner/v1', json={"token":token1,"channel_id":channel_id,"u_id":u_id3}).status_code
+    assert output == 200
+    output2 = requests.get(config.url + 'channel/details/v2' + f'?token={token2}&channel_id={channel_id}')
+    assert output2.status_code == 200
+    assert len(json.loads(output2.text)['all_members']) == 2
+    assert len(json.loads(output2.text)['owner_members']) == 2
 
 #############################################################################
 #                                                                           #
@@ -399,6 +438,30 @@ def test_channel_removeowner_invalid_token_http(user1,user2):
 #            http test for channel_removeowner Global exception             #
 #                                                                           #
 #############################################################################
+
+
+def test_channel_removeowner_global_owner_exception_http(user1,user2,user3):
+    requests.delete(config.url + 'clear/v1')
+    user1_register = requests.post(config.url + 'auth/register/v2', json=user1)
+    user2_register = requests.post(config.url + 'auth/register/v2', json=user2)
+    user3_register = requests.post(config.url + 'auth/register/v2', json=user3)
+    user1_login = requests.post(config.url + 'auth/login/v2', json=user1)
+    user2_login = requests.post(config.url + 'auth/login/v2', json=user2)
+    user3_login = requests.post(config.url + 'auth/login/v2', json=user3)
+    token1 = json.loads(user1_login.text).get('token')
+    token2 = json.loads(user2_login.text).get('token')
+    token3 = json.loads(user3_login.text).get('token')
+    u_id3 = json.loads(user3_login.text).get('auth_user_id')
+    channel = requests.post(config.url + 'channels/create/v2', json={"token":token2,"name":"channelone","is_public":True})
+    channel_id = json.loads(channel.text).get('channel_id')
+    requests.post(config.url + 'channel/join/v2', json={"token":token3,"channel_id":channel_id}).status_code
+    requests.post(config.url + 'channel/addowner/v1', json={"token":token2,"channel_id":channel_id,"u_id":u_id3})
+    output = requests.post(config.url + 'channel/removeowner/v1', json={"token":token1,"channel_id":channel_id,"u_id":u_id3}).status_code
+    assert output == 200
+    output2 = requests.get(config.url + 'channel/details/v2' + f'?token={token2}&channel_id={channel_id}')
+    assert output2.status_code == 200
+    assert len(json.loads(output2.text)['all_members']) == 2
+    assert len(json.loads(output2.text)['owner_members']) == 1
 
 
 #############################################################################
@@ -515,3 +578,74 @@ def test_channels_create_invalid_token_http(user1):
     invalid_token = f"{token1}123"
     output = requests.post(config.url + 'channels/create/v2', json={"token":invalid_token,"name":"channelone","is_public":True}).status_code
     assert output == 403
+
+
+#############################################################################
+#                                                                           #
+#         http test for valid channel and channels implementation           #
+#                                                                           #
+#############################################################################
+
+
+def test_channel_channels_valid_implementation(user1,user2,user3):
+    requests.delete(config.url + 'clear/v1')
+    user1_register = requests.post(config.url + 'auth/register/v2', json=user1)
+    user2_register = requests.post(config.url + 'auth/register/v2', json=user2)
+    user3_register = requests.post(config.url + 'auth/register/v2', json=user3)
+    user1_login = requests.post(config.url + 'auth/login/v2', json=user1)
+    user2_login = requests.post(config.url + 'auth/login/v2', json=user2)
+    user3_login = requests.post(config.url + 'auth/login/v2', json=user3)
+    token1 = json.loads(user1_login.text).get('token')
+    token2 = json.loads(user2_login.text).get('token')
+    token3 = json.loads(user3_login.text).get('token')
+    u_id2 = json.loads(user2_login.text).get('auth_user_id')
+
+    # Test valid channels_create function
+    create_channel = requests.post(config.url + 'channels/create/v2', json={"token":token1,"name":"channelone","is_public":True})
+    channel_id = json.loads(create_channel.text).get('channel_id')
+    assert create_channel.status_code == 200
+
+    # Test valid channels_listall function and test create_channel created a channel 
+    channels_listall_test = requests.get(config.url + 'channels/listall/v2' + f'?token={token1}')
+    assert json.loads(channels_listall_test.text).get('channels')[0]['channel_id'] == channel_id
+    assert channels_listall_test.status_code == 200
+
+    # Test valid channel_join and channel_invite function
+    channel_invite_test = requests.post(config.url + 'channel/invite/v2', json={"token":token1,"channel_id":channel_id,"u_id":u_id2})
+    assert channel_invite_test.status_code == 200
+    channel_join_test = requests.post(config.url + 'channel/join/v2', json={"token":token3,"channel_id":channel_id})
+    assert channel_join_test.status_code == 200
+
+    # Test valid channels_list function and test user2 is in channel
+    channels_list_test = requests.get(config.url + 'channels/list/v2' + f'?token={token2}')
+    assert json.loads(channels_list_test.text).get('channels')[0]['channel_id'] == channel_id
+    assert channels_list_test.status_code == 200
+
+    # Test valid channel_details function and test user2 and user3 is in channel
+    channel_details_test = requests.get(config.url + 'channel/details/v2' + f'?token={token3}&channel_id={channel_id}')
+    assert channel_details_test.status_code == 200
+    assert json.loads(channel_details_test.text).get('name') == "channelone"
+    assert json.loads(channel_details_test.text).get('is_public') == True
+    assert len(json.loads(channel_details_test.text).get('all_members')) == 3
+    assert len(json.loads(channel_details_test.text).get('owner_members')) == 1
+
+    # Test valid channel_addowner function
+    channel_addowner_test = requests.post(config.url + 'channel/addowner/v1', json={"token":token1,"channel_id":channel_id,"u_id":u_id2})
+    assert channel_addowner_test.status_code == 200
+    details1 = requests.get(config.url + 'channel/details/v2' + f'?token={token1}&channel_id={channel_id}')
+    assert len(json.loads(details1.text).get('owner_members')) == 2
+    assert len(json.loads(details1.text).get('all_members')) == 3
+
+    # Test valid channel_removeowner function
+    channel_removeowner_test = requests.post(config.url + 'channel/removeowner/v1', json={"token":token1,"channel_id":channel_id,"u_id":u_id2})
+    assert channel_removeowner_test.status_code == 200
+    details2 = requests.get(config.url + 'channel/details/v2' + f'?token={token1}&channel_id={channel_id}')
+    assert len(json.loads(details2.text).get('owner_members')) == 1
+    assert len(json.loads(details2.text).get('all_members')) == 3
+
+    # Test valid channel_leave function
+    channel_leave_test = requests.post(config.url + 'channel/leave/v1', json={"token":token3,"channel_id":channel_id})
+    assert channel_leave_test.status_code == 200
+    details3 = requests.get(config.url + 'channel/details/v2' + f'?token={token1}&channel_id={channel_id}')
+    assert len(json.loads(details3.text).get('owner_members')) == 1
+    assert len(json.loads(details3.text).get('all_members')) == 2
