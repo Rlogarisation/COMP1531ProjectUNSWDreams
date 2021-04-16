f2e8832498f207e34f3a0ac82a74663dff728db0
Shi Tong Yuan <z5192519@ad.unsw.edu.au>
2021-04-16 22:00:10 +0800

    bonus: asciimoji import & export & tests

--- src/bonus.py
+++ src/bonus.py
@@ -1,1 +1,26 @@
 # Here is bonus functions
+from src.data_file import data
+from src.other import InputError, AccessError
+from src.auth import get_user_by_token
+
+
+def asciimoji_import(token):
+    user = get_user_by_token(token)
+    user_name = user.name_first + user.name_last
+    try:
+        import_file = open(f"{user_name}_asciimoji.txt", "r")
+        import_asciimoji = eval(import_file.read())
+    except FileNotFoundError:
+        warning = f"asciimoji : could not find your personal asciimoji file {user_name}_asciimoji.txt."
+        raise InputError(description=warning)
+    user.asciimoji.update(import_asciimoji)
+    import_file.close()
+
+
+def asciimoji_export(token):
+    user = get_user_by_token(token)
+    user_name = user.name_first + user.name_last
+
+    export_file = open(f"{user_name}_asciimoji.txt", "w")
+    export_file.write(str(user.asciimoji))
+    export_file.close()
--- tests/bonus_test.py
+++ tests/bonus_test.py
@@ -2,10 +2,11 @@
 from src.bonus import asciimoji_export, asciimoji_import
 from src.message import message_send_v2, message_senddm_v1
 from src.other import clear_v1
-from src.auth import auth_register_v1, auth_login_v1
+from src.auth import auth_register_v1, auth_login_v1, get_user_by_token
 from src.channels import channels_create_v1
 from src.channel import channel_messages_v1
 from src.dm import dm_create_v1, dm_messages_v1
+import os
 
 
 def test_send_asciimoji_to_channel():
@@ -21,7 +22,7 @@ def test_send_asciimoji_to_channel():
     channel_msgs = channel_messages_v1(token_0, channel_0_id, 0)
     assert len(channel_msgs['messages']) == 1
     assert channel_msgs['messages'][0]['message'] == "⊂(◉‿◉)つ i am acid, lol."
-    clear_v1
+    clear_v1()
 
 
 def test_send_asciimoji_to_dm():
@@ -39,4 +40,37 @@ def test_send_asciimoji_to_dm():
     dm_msgs = dm_messages_v1(token_0, dm_0_id, 0)
     assert len(dm_msgs['messages']) == 1
     assert dm_msgs['messages'][0]['message'] == "⊂(◉‿◉)つ i am acid, lol."
-    clear_v1
+    clear_v1()
+
+
+def test_asciimoji_import():
+    clear_v1()
+
+    token_0 = auth_register_v1("test_email0@gmail.com", "password", "First0", "Last0")["token"]
+
+    user_0 = get_user_by_token(token_0)
+    user_name = user_0.name_first + user_0.name_last
+    extra_asciimoji = open(f"{user_name}_asciimoji.txt", "w")
+    extra_asciimoji.write(str({"<creep>": "ԅ(≖‿≖ԅ)", "crim3s": "( ✜︵✜ )", "cute": "(｡◕‿‿◕｡)"}))
+    extra_asciimoji.close()
+
+    asciimoji_import(token_0)
+
+    assert user_0.asciimoji == {"<acid>": "⊂(◉‿◉)つ", "<afraid>": "(ㆆ _ ㆆ)", "<angry>": "•`_´•", "<catlenny>": "( ͡° ᴥ ͡°)", "<creep>": "ԅ(≖‿≖ԅ)", "crim3s": "( ✜︵✜ )", "cute": "(｡◕‿‿◕｡)"}
+    os.system(f"rm -rf {user_name}_asciimoji.txt")
+    clear_v1()
+
+
+def test_asciimoji_export():
+    clear_v1()
+    token_1 = auth_register_v1("test_email1@gmail.com", "password", "First1", "Last1")["token"]
+
+    user_1 = get_user_by_token(token_1)
+    asciimoji_export(token_1)
+
+    user_name = user_1.name_first + user_1.name_last
+    check_export = open(f"{user_name}_asciimoji.txt", "r")
+
+    assert check_export.read() == str({"<acid>": "⊂(◉‿◉)つ", "<afraid>": "(ㆆ _ ㆆ)", "<angry>": "•`_´•", "<catlenny>": "( ͡° ᴥ ͡°)"})
+    os.system(f"rm -rf {user_name}_asciimoji.txt")
+    clear_v1()
