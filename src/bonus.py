cab9be5b7913078d4974470e67bcea359082febf
Shi Tong Yuan <z5192519@ad.unsw.edu.au>
2021-04-16 22:47:22 +0800

    bonus: asciimoji add type pak and txt

--- src/bonus.py
+++ src/bonus.py
@@ -2,25 +2,38 @@
 from src.data_file import data
 from src.other import InputError, AccessError
 from src.auth import get_user_by_token
+import pickle
 
 
-def asciimoji_import(token):
+def asciimoji_import_package(token, package, pkg_type):
     user = get_user_by_token(token)
-    user_name = user.name_first + user.name_last
-    try:
-        import_file = open(f"{user_name}_asciimoji.txt", "r")
-        import_asciimoji = eval(import_file.read())
-    except FileNotFoundError:
-        warning = f"asciimoji : could not find your personal asciimoji file {user_name}_asciimoji.txt."
-        raise InputError(description=warning)
-    user.asciimoji.update(import_asciimoji)
-    import_file.close()
+    if pkg_type == "pak":
+        try:
+            with open(package + ".pak", "rb") as FILE:
+                asciimoji_load = pickle.load(FILE)
+        except FileNotFoundError:
+            raise InputError(description=f"asciimoji_import_package : {package}.pak not found.")
+    elif pkg_type == "txt":
+        try:
+            with open(package + ".txt", "r") as FILE:
+                asciimoji_load = FILE.read()
+        except FileNotFoundError:
+            raise InputError(description=f"asciimoji_import_package : {package}.txt not found.")
+
+    user.asciimoji.update(eval(asciimoji_load))
+    FILE.close()
 
 
-def asciimoji_export(token):
+def asciimoji_export_package(token, package, pkg_type):
     user = get_user_by_token(token)
-    user_name = user.name_first + user.name_last
 
-    export_file = open(f"{user_name}_asciimoji.txt", "w")
-    export_file.write(str(user.asciimoji))
-    export_file.close()
+    asciimoji_saved = str(user.asciimoji)
+
+    if pkg_type == "pak":
+        with open(package + ".pak", "wb") as FILE:
+            pickle.dump(asciimoji_saved, FILE)
+    elif pkg_type == "txt":
+        with open(package + ".txt", "w") as FILE:
+            FILE.write(asciimoji_saved)
+
+    FILE.close()
--- tests/bonus_test.py
+++ tests/bonus_test.py
@@ -1,5 +1,5 @@
 # Here is the tests for Bonus functions
-from src.bonus import asciimoji_export, asciimoji_import
+from src.bonus import asciimoji_import_package, asciimoji_export_package
 from src.message import message_send_v2, message_senddm_v1
 from src.other import clear_v1
 from src.auth import auth_register_v1, auth_login_v1, get_user_by_token
@@ -7,6 +7,7 @@ from src.channels import channels_create_v1
 from src.channel import channel_messages_v1
 from src.dm import dm_create_v1, dm_messages_v1
 import os
+import pickle
 
 
 def test_send_asciimoji_to_channel():
@@ -43,34 +44,71 @@ def test_send_asciimoji_to_dm():
     clear_v1()
 
 
-def test_asciimoji_import():
+def test_asciimoji_import_txt():
     clear_v1()
 
     token_0 = auth_register_v1("test_email0@gmail.com", "password", "First0", "Last0")["token"]
 
     user_0 = get_user_by_token(token_0)
     user_name = user_0.name_first + user_0.name_last
-    extra_asciimoji = open(f"{user_name}_asciimoji.txt", "w")
+
+    package_name = user_name
+    extra_asciimoji = open(package_name + ".txt", "w")
     extra_asciimoji.write(str({"<creep>": "ԅ(≖‿≖ԅ)", "crim3s": "( ✜︵✜ )", "cute": "(｡◕‿‿◕｡)"}))
     extra_asciimoji.close()
 
-    asciimoji_import(token_0)
+    asciimoji_import_package(token_0, package_name, "txt")
 
     assert user_0.asciimoji == {"<acid>": "⊂(◉‿◉)つ", "<afraid>": "(ㆆ _ ㆆ)", "<angry>": "•`_´•", "<catlenny>": "( ͡° ᴥ ͡°)", "<creep>": "ԅ(≖‿≖ԅ)", "crim3s": "( ✜︵✜ )", "cute": "(｡◕‿‿◕｡)"}
-    os.system(f"rm -rf {user_name}_asciimoji.txt")
+    os.system(f"rm -rf {package_name}.txt")
     clear_v1()
 
 
-def test_asciimoji_export():
+def test_asciimoji_export_txt():
     clear_v1()
     token_1 = auth_register_v1("test_email1@gmail.com", "password", "First1", "Last1")["token"]
 
     user_1 = get_user_by_token(token_1)
-    asciimoji_export(token_1)
+    user_name = package_name = user_1.name_first + user_1.name_last
 
-    user_name = user_1.name_first + user_1.name_last
-    check_export = open(f"{user_name}_asciimoji.txt", "r")
+    asciimoji_export_package(token_1, package_name, "txt")
+
+    check_export = open(package_name + ".txt", "r")
 
     assert check_export.read() == str({"<acid>": "⊂(◉‿◉)つ", "<afraid>": "(ㆆ _ ㆆ)", "<angry>": "•`_´•", "<catlenny>": "( ͡° ᴥ ͡°)"})
-    os.system(f"rm -rf {user_name}_asciimoji.txt")
+    os.system(f"rm -rf {package_name}.txt")
+    clear_v1()
+
+
+def test_asciimoji_import_pak():
+    clear_v1()
+    token_1 = auth_register_v1("test_email1@gmail.com", "password", "First1", "Last1")["token"]
+    user_1 = get_user_by_token(token_1)
+
+    asciimoji_saved = str({"<creep>": "ԅ(≖‿≖ԅ)", "crim3s": "( ✜︵✜ )", "cute": "(｡◕‿‿◕｡)"})
+    package = "asciimoji"
+    with open(package + ".pak", "wb") as FILE:
+        pickle.dump(asciimoji_saved, FILE)
+    FILE.close()
+
+    asciimoji_import_package(token_1, package, "pak")
+
+    assert user_1.asciimoji == {"<acid>": "⊂(◉‿◉)つ", "<afraid>": "(ㆆ _ ㆆ)", "<angry>": "•`_´•", "<catlenny>": "( ͡° ᴥ ͡°)", "<creep>": "ԅ(≖‿≖ԅ)", "crim3s": "( ✜︵✜ )", "cute": "(｡◕‿‿◕｡)"}
+    os.system(f"rm -rf {package}.pak")
+    clear_v1()
+
+
+def test_asciimoji_export_pak():
+    clear_v1()
+    token_1 = auth_register_v1("test_email1@gmail.com", "password", "First1", "Last1")["token"]
+    user_1 = get_user_by_token(token_1)
+
+    package = "asciimoji"
+    asciimoji_export_package(token_1, package, "pak")
+
+    with open(package + ".pak", "rb") as FILE:
+        asciimoji_load = pickle.load(FILE)
+    assert eval(asciimoji_load) == {"<acid>": "⊂(◉‿◉)つ", "<afraid>": "(ㆆ _ ㆆ)", "<angry>": "•`_´•", "<catlenny>": "( ͡° ᴥ ͡°)"}
+
+    os.system(f"rm -rf {package}.pak")
     clear_v1()
