54ac5aef338eab9b236fe0f95047b4dfaa2db494
Shi Tong Yuan <z5192519@ad.unsw.edu.au>
2021-04-20 11:52:58 +0800

    add alicloud deploy settings

--- src/config.py
+++ src/config.py
@@ -1,3 +1,7 @@
 port = 8080
 
-url = f"http://localhost:{port}/"
+# loca deploy
+# url = f"http://localhost:{port}/"
+
+# deployed to AliCloud
+url = f"http://101.132.122.16:{port}/"
---
+++ src/deploy_ali.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+WORKING_DIRECTORY="~/www/cs1531deploy"
+
+USERNAME="yst990102"
+SSH_HOST="101.132.122.16"
+
+rm -rf ./**/__pycache__ ./**/.pytest_cache > /dev/null
+scp -r ./requirements.txt ./src "$USERNAME@$SSH_HOST:$WORKING_DIRECTORY"
+ssh "$USERNAME@$SSH_HOST" "cd $WORKING_DIRECTORY && source bin/activate && pip3 install -r requirements.txt"
